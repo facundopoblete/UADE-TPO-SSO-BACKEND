90cb119d2b01fa559f00122def02908e6367b47f
Facundo Poblete <facundo.poblete@mulesoft.com>
2019-08-15 20:07:06 -0300

    First commit - Health endpoint - Swagger doc

---
+++ Api/Controllers/HealthController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers
+{
+    [Route("api/[controller]")]
+    public class HealthController : Controller
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return "pong";
+        }
+    }
+}
---
+++ Api/Startup.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using Swashbuckle.AspNetCore.Swagger;
+
+namespace Api
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSwaggerGen(swaggerGen => swaggerGen.SwaggerDoc("v1", new Info { Title = "UADE SSO", Version = "v1" }));
+
+            services.AddCors(o => o.AddPolicy("CorsPolicy", builder => {
+                builder
+                .AllowAnyMethod()
+                .AllowAnyHeader()
+                .AllowAnyOrigin();
+            }));
+
+            services.AddMvc();
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseStaticFiles();
+
+            app.UseAuthentication();
+
+            app.UseDeveloperExceptionPage();
+
+            app.UseCors("CorsPolicy");
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+
+            app.UseSwagger();
+
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "UADE SSO");
+            });
+        }
+    }
+}
